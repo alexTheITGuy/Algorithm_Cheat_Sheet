8ede4e97fae27565e5ca9effe4490f3077755ca2
Alex Ochoa <aaochoa6@asu.edu>
2025-07-30 15:11:23 -0600

    Update Sorting_Algos.py

--- Sorting_Algos.py
+++ Sorting_Algos.py
@@ -4,8 +4,8 @@ import random
 myIntList = []
 number = 0
 
-for i in range(20000):
-    random_integer = random.randint(1, 10000)
+for i in range(100):
+    random_integer = random.randint(1, 10)
     myIntList.append(random_integer)
 
 #Bubble Sort
@@ -13,16 +13,28 @@ def bubble_sort(list):
     for i in range(len(list)):
         for j in range(0, len(list) - i - 1):
 
-      
             if list[j] > list[j + 1]:
                 temp = list[j]
                 list[j] = list[j+1]
                 list[j+1] = temp
     print(list)
+#Selection Sort
+def selection_sort(list):
+    for scanIndex in range(0, len(list)):
+        minIndex = scanIndex
+
+        for compIndex in range(scanIndex + 1, len(list)):
+            if list[compIndex] < list[minIndex]:
+                minIndex = compIndex
+
+        if minIndex != scanIndex:
+                list[scanIndex], list[minIndex] = list[minIndex], list[scanIndex]
+
+                print(list)
 
 #Menu to Select Algo to run... maybe later ill add run time information. 
-print('1 = Bubble Sort')
-print('2 = Selection Sort')
+print('1 = Bubble Sort O(n^2)')
+print('2 = Selection Sort O(n^2)')
 print('3 = Insertion Sort')
 print('4 = Merge Sort')
 print('5 = Quick Sort')
@@ -37,13 +49,9 @@ algo_selection = int(input('Select the algo to run: '))
 match algo_selection :
     case 1:
         bubble_sort(myIntList)
-
-
-
-
-
-
-
+    
+    case 2:
+        selection_sort(myIntList)
 
 
 
